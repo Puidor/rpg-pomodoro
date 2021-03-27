4a18d7c94b9891cd332b793aff0a93e14eedd90d
Puidor <c.adriano.silva@hotmail.com>
2021-03-27 12:02:22 -0300

    25 Minutes

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -46,7 +46,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }
 
